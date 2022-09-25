0bbc680ea9e4ffb4233072cdbb3919cf740fa9d3
SeokAm Moon <liendmch@gmail.com>
2022-09-26 01:54:51 +0900

    add: Main Page

---
+++ src/components/BloggerInfo/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+const Wrapper = styled.div`
+  margin-bottom: 50px;
+  height: 400px;
+`;
+
+const WordWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  padding: 10px;
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+`;
+
+const Word = styled.div``;
+
+const SocialLink = styled.span`
+  display: flex;
+  padding: 10px;
+`;
+
+const FontAwesome = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  opacity: 0.6;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  padding-left: 10px;
+`;
+
+const WordStrong = styled.span`
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+`;
+
+export const BloggerInfo = () => {
+  return (
+    <Wrapper>
+      <WordWrapper>
+        <Word>
+          안녕하세요.
+          <br /> 개발자<WordStrong> 문석암 </WordStrong>입니다.
+        </Word>
+        <SocialLink>
+          <FontAwesome
+            onClick={() => {
+              window.open('https://github.com/mon823');
+            }}
+            icon={faGithub as IconProp}
+          />
+          <FontAwesome
+            onClick={() => {
+              window.open('mailto:liendmch@gmail.com');
+            }}
+            icon={faEnvelope as IconProp}
+          />
+        </SocialLink>
+      </WordWrapper>
+      Latest
+    </Wrapper>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,21 @@ import type { HeadFC } from 'gatsby';
 import '@styles/typography.css';
 import Layout from '@layouts/MainLayout';
 import { RouteComponentProps } from '@reach/router';
+import { getAllData } from '@/hooks/getAllData';
+import { BoardCard } from '@/components/BoardCard';
+import { BloggerInfo } from '@/components/BloggerInfo';
 
 const IndexPage = (props: RouteComponentProps) => {
-  return <Layout></Layout>;
+  const result = getAllData('', 'category').splice(0, 5);
+
+  return (
+    <Layout>
+      <BloggerInfo></BloggerInfo>
+      {result.map(data => {
+        return <BoardCard key={data.node.frontmatter.slug} node={data.node}></BoardCard>;
+      })}
+    </Layout>
+  );
 };
 
 export default IndexPage;
