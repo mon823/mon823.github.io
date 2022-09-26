ae6b411533519da514ff829ab6cea1f2e985a6d8
SeokAm Moon <liendmch@gmail.com>
2022-09-26 22:03:35 +0900

    add: 특정 해상도에서 홈버튼 변경

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, navigate } from 'gatsby';
+import Logo from '@/images/logo.svg';
+import { navigate } from 'gatsby';
 
 const Wrapper = styled.nav`
   padding-bottom: 20px;
   display: flex;
+  height: 50px;
   font-family: 'SourceHanSerif';
   font-size: ${({ theme }) => theme.fontSize.lg};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
+  .logo {
+    display: none;
+  }
+  @media screen and (max-width: 499px) {
+    font-size: ${({ theme }) => theme.fontSize.xs};
+  }
+  @media screen and (max-width: 273px) {
+    .logo {
+      display: block;
+      background-image: url(${Logo});
+      width: 200px;
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+  }
 `;
 
-const Icon = styled(Link)`
+const Icon = styled.div`
   justify-content: center;
   all: unset;
   cursor: pointer;
-  @media screen and (max-width: 499px) {
+  @media screen and (max-width: 273px) {
     display: none;
+    font-size: 0;
   }
 `;
 
@@ -35,7 +53,19 @@ const NavMenu = styled.button`
 const Nav = () => {
   return (
     <Wrapper>
-      <Icon to="/">Life Vending Machine</Icon>
+      <Icon
+        onClick={() => {
+          void navigate('/');
+        }}
+      >
+        Life Vending Machine
+      </Icon>
+      <div
+        className="logo"
+        onClick={() => {
+          void navigate('/');
+        }}
+      ></div>
       <NavMenuBox>
         <NavMenu>about </NavMenu>
         <NavMenu
