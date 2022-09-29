85bc7549324c612c92147e0c9a6d85bae1be96f5
SeokAm Moon <liendmch@gmail.com>
2022-09-30 05:47:40 +0900

    hotfix: 배포후에만 발생하는 에러......

--- src/components/PostToc/styleComponents.tsx
+++ src/components/PostToc/styleComponents.tsx
@@ -1,54 +1,54 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-  position: fixed;
-  z-index: 1000;
-  top: 150px;
-  right: 0px;
-  width: 300px;
-  margin-right: 20px;
-  font-size: ${({ theme }) => theme.fontSize.sm};
+// export const Wrapper = styled.div`
+//   position: fixed;
+//   z-index: 1000;
+//   top: 150px;
+//   right: 0px;
+//   width: 300px;
+//   margin-right: 20px;
+//   font-size: ${({ theme }) => theme.fontSize.sm};
 
-  @media screen and (max-width: 1720px) {
-    width: 200px;
-  }
-  @media screen and (max-width: 1520px) {
-    position: relative;
-    border-radius: 3px;
-    width: 100%;
-    top: 0px;
-    padding: 10px;
-    margin-bottom: 20px;
-    font-size: ${({ theme }) => theme.fontSize.md};
-    border: 1px solid ${({ theme }) => theme.color.lineColor};
-    .markdown-toc {
-      display: none;
-    }
-  }
-  @media screen and (min-width: 1521px) {
-    .markdown-toc {
-      display: block;
-    }
-  }
-`;
-export const HeaderWrapper = styled.div`
-  display: flex;
-  cursor: pointer;
-  font-size: ${({ theme }) => theme.fontSize.xl};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-`;
+//   @media screen and (max-width: 1720px) {
+//     width: 200px;
+//   }
+//   @media screen and (max-width: 1520px) {
+//     position: relative;
+//     border-radius: 3px;
+//     width: 100%;
+//     top: 0px;
+//     padding: 10px;
+//     margin-bottom: 20px;
+//     font-size: ${({ theme }) => theme.fontSize.md};
+//     border: 1px solid ${({ theme }) => theme.color.lineColor};
+//     .markdown-toc {
+//       display: none;
+//     }
+//   }
+//   @media screen and (min-width: 1521px) {
+//     .markdown-toc {
+//       display: block;
+//     }
+//   }
+// `;
+// export const HeaderWrapper = styled.div`
+//   display: flex;
+//   cursor: pointer;
+//   font-size: ${({ theme }) => theme.fontSize.xl};
+//   font-weight: ${({ theme }) => theme.fontWeight.bold};
+// `;
 
-export const Word = styled.p`
-  margin-right: auto;
-`;
+// export const Word = styled.p`
+//   margin-right: auto;
+// `;
 
-export const Btn = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.xl};
-`;
+// export const Btn = styled.p`
+//   font-size: ${({ theme }) => theme.fontSize.xl};
+// `;
 
-export const UpScrollBtn = styled.div`
-  cursor: pointer;
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-  margin-left: 7px;
-  margin-top: 20px;
-`;
+// export const UpScrollBtn = styled.div`
+//   cursor: pointer;
+//   font-weight: ${({ theme }) => theme.fontWeight.bold};
+//   margin-left: 7px;
+//   margin-top: 20px;
+// `;
